70bb086f567901f2f320cc890f5925196686a345
yewton <yewton@gmail.com>
2018-01-24 10:03:09 +0900

    wip

--- lisp/ytn-init-common.el
+++ lisp/ytn-init-common.el
@@ -163,8 +163,11 @@
   :bind (:map dired-mode-map
               ("r" . wdired-change-to-wdired-mode)))
 
-
-
+(use-package sh-script
+  :defer t
+  :config
+  (setq sh-basic-offset 2
+        sh-indentation 2))
 
 ;; ------ vendor -------
 
