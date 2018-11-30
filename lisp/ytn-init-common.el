40bf9df0c75b7618d956e47a0ccc23a068732db0
yewton <yewton@gmail.com>
2018-11-30 11:21:48 +0900

    :art: agenda 表示をにぎやかに

--- lisp/ytn-init-common.el
+++ lisp/ytn-init-common.el
@@ -442,8 +442,8 @@ If called in a project it calls `treemacs-projectile', otherwise `treemacs'."
         org-startup-folded 'content
         org-agenda-start-on-weekday nil
         org-attach-store-link-p t
-        org-agenda-deadline-leaders '("⏰ " "%3d日後: " "%2d日前: ")
-        org-agenda-scheduled-leaders '("📅 " "Sched.%2dx: ")
+        org-agenda-deadline-leaders '("⏰" "🔜%2dd" "⚠%2dd")
+        org-agenda-scheduled-leaders '("📅" "📌%2dd")
         org-agenda-format-date "%_2m月%e日(%a) %_20Y年"
         org-agenda-show-current-time-in-grid t
         org-agenda-current-time-string "▷ - - - - - - - - - - - - - - - - - - - - - - - - -"))
