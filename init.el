df51d77174efd408b7a044c813d5c1b795f37514
yewton <yewton@gmail.com>
2019-06-20 00:08:05 +0900

    :sparkles: *Message* にタイムスタンプ付与

--- init.el
+++ init.el
@@ -3,6 +3,29 @@
 ;; cf. https://www.reddit.com/r/emacs/comments/56fvgd/is_there_a_way_to_stop_emacs_from_adding_the/
 (setq package--init-file-ensured t)
 
+;; https://emacs.stackexchange.com/a/33523/18118
+(defun sh/current-time-microseconds ()
+  "Return the current time formatted to include microseconds."
+  (let* ((nowtime (current-time))
+         (now-ms (nth 2 nowtime)))
+    (concat (format-time-string "[%Y-%m-%dT%T" nowtime) (format ".%d]" now-ms))))
+
+(defun sh/ad-timestamp-message (FORMAT-STRING &rest args)
+  "Advice to run before `message' that prepends a timestamp to each message.
+
+Activate this advice with:
+(advice-add 'message :before 'sh/ad-timestamp-message)"
+  (unless (string-equal FORMAT-STRING "%s%s")
+    (let ((deactivate-mark nil)
+          (inhibit-read-only t))
+      (with-current-buffer "*Messages*"
+        (goto-char (point-max))
+        (if (not (bolp))
+          (newline))
+        (insert (sh/current-time-microseconds) " ")))))
+
+(advice-add 'message :before 'sh/ad-timestamp-message)
+
 (when load-file-name
   (setq user-emacs-directory (file-name-directory load-file-name)))
 (setq custom-file (expand-file-name "custom.el" user-emacs-directory))
