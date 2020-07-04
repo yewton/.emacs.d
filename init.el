d3bbdc25aecac09217da0433fdc137067a3c3fd4
yewton <yewton@gmail.com>
2020-07-05 04:19:50 +0900

    🚀 GC の設定を追加する

--- init.el
+++ init.el
@@ -1,5 +1,8 @@
 ;; -*- lexical-binding: t; -*-
 
+;; cf. https://blog.d46.us/advanced-emacs-startup/#org37ca53c
+(setq gc-cons-threshold (* 50 1000 1000))
+
 ;; cf. https://www.reddit.com/r/emacs/comments/56fvgd/is_there_a_way_to_stop_emacs_from_adding_the/
 (setq package--init-file-ensured t)
 
@@ -40,6 +43,8 @@
 (load "ytn-config")
 
 (load custom-file 'noerror)
+
+(setq gc-cons-threshold (* 2 1000 1000))
 
 ;; Local Variables:
 ;; no-byte-compile: t
