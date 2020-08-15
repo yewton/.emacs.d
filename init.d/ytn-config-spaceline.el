574d30a7367b35466190250a2c0dccd5fdfd1147
yewton <yewton@gmail.com>
2020-08-16 04:37:39 +0900

    🤕 Attempt to fix https://github.com/milkypostman/powerline/issues/37
    Shamelessly ported from https://github.com/tarsius/moody

--- init.d/ytn-config-spaceline.el
+++ init.d/ytn-config-spaceline.el
@@ -59,7 +59,36 @@
       (buffer-position :priority 0)
       (hud :priority 0))))
 
+;;; Hack
+
+;; see https://github.com/tarsius/moody/commit/8f96f1ec6b331747774d84a6cc49968503bac7d0
+(defun powerline-set-selected-window1 (&optional _)
+  (if (null (frame-focus-state))
+    (setq powerline-selected-window nil)
+    (let ((win (selected-window)))
+      (unless (minibuffer-window-active-p win)
+        (setq powerline-selected-window win)))))
+(add-hook 'pre-redisplay-functions #'powerline-set-selected-window1)
+(add-function :after after-focus-change-function #'powerline-set-selected-window1)
+
+;; see https://github.com/tarsius/moody/commit/a7fb64d6fae15ed6ff87e540ff177134fc0b19b5
+(defvar-local powerline--size-hacked-p nil)
+(defun powerline-redisplay (&optional _force &rest _ignored)
+  "See `moody-redisplay'"
+  (when (and mode-line-format (not powerline--size-hacked-p))
+    (setq powerline--size-hacked-p t)
+    (redisplay t)))
+(advice-add 'fit-window-to-buffer :before #'powerline-redisplay)
+
 (setq-default mode-line-format '("%e" (:eval (spaceline-ml-main))))
+
+(remove-hook 'window-configuration-change-hook #'powerline-set-selected-window)
+(remove-hook 'buffer-list-update-hook #'powerline-set-selected-window)
+(ad-disable-advice 'handle-switch-frame 'after 'powerline-handle-switch-frame)
+;; https://github.com/TheBB/spaceline/issues/110#issuecomment-283513541
+(with-no-warnings
+  (remove-hook 'focus-out-hook #'powerline-unset-selected-window)
+  (remove-hook 'focus-in-hook #'powerline-set-selected-window))
 
 (add-hook 'window-setup-hook #'ytn-spaceline-theme)
 ;; Local Variables:
